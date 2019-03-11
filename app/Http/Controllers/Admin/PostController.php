c95d00d8f70fdddfe860be1097b073e37ca0400b
JenikD <renegate1984@gmail.com>
2019-03-11 10:20:21 +0200

    Add: Добавлен CRUD категорий и частично постов

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Post;
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.posts.index', [
+          'posts' => Post::orderBy('created_at', 'desc')->paginate(10)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create', [
+          'post'    => [],
+          'categories' => Category::with('children')->where('parent_id', 0)->get(),
+          'delimiter'  => ''
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = Post::create($request->all());
+
+        // Categories
+        if($request->input('categories')) :
+          $post->categories()->attach($request->input('categories'));
+        endif;
+
+        return redirect()->route('admin.post.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', [
+          'post'    => $post,
+          'categories' => Category::with('children')->where('parent_id', 0)->get(),
+          'delimiter'  => ''
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $post->update($request->except('slug'));
+
+        // Categories
+        $post->categories()->detach();
+        if($request->input('categories')) :
+          $post->categories()->attach($request->input('categories'));
+        endif;
+
+        return redirect()->route('admin.post.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->categories()->detach();
+        $post->delete();
+
+        return redirect()->route('admin.post.index');
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    // Mass assigned
+    protected $fillable = ['title', 'slug', 'description_short', 'description', 'image', 'image_show', 'meta_title', 'meta_description', 'meta_keyword', 'published', 'created_by', 'modified_by'];
+
+    // Mutators
+    public function setSlugAttribute($value) {
+      $this->attributes['slug'] = Str::slug( mb_substr($this->title, 0, 40) . "-" . \Carbon\Carbon::now()->format('dmyHi'), '-');
+    }
+    
+    // Polymorphic relation with categories
+    public function categories()
+    {
+      return $this->morphToMany('App\Category', 'categoryable');
+    }
+}
